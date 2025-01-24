f03525adc66755ee647f537b5f1dd29ebfc9a152
sina7843 <sinakh7843@gmail.com>
2025-01-24 12:19:40 +0330

    projectile bubble

--- gun/projectile.gd
+++ gun/projectile.gd
@@ -1,8 +1,20 @@
 extends Node2D
 class_name Projectile
 
-const SPEED: int = 300
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+const SPEED: int = 200
 var direction_hit: int = 1
 
 func _process(delta: float) -> void:
 	position += direction_hit * transform.x * SPEED * delta
+	if position.x > 150 : 
+		queue_free()
+	if position.x <= 120:
+		var random_number = randf_range(-1, 1)
+		position.y += random_number
+		clamp(position.y, position.y-1, position.y)
+	elif position.x > 120:
+		var random_number = randf_range(-5, 5)
+		position.y += random_number
+		clamp(position.y, position.y-4, position.y)
